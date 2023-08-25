5c538f9aeec89d9b1bbfaf51c88a221732e5b3cc
polamjag <s@polamjag.info>
2023-08-25 23:28:44 +0900

    it works

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import OpenLocationCode from "open-location-code-typescript";
+
+const queryGsi = async (address: string) => {
+  const res = await fetch(
+    `https://msearch.gsi.go.jp/address-search/AddressSearch?q=${encodeURIComponent(
+      address
+    )}`
+  );
+  const resJson = await res.json();
+  const [lon, lat]: [number, number] = resJson[0].geometry.coordinates;
+  return { lon, lat };
+};
+
+const splitLocalPlusCode = (localPlusCode: string) => {
+  const match = localPlusCode.match(/([A-Za-z0-9]+\+[A-Za-z0-9+]+)\s+(.*)/);
+  return { localPlusCode: match?.[1], address: match?.[2] };
+};
+
+const yjcarnaviUrlFromPlusCode = async (plusCode: string) => {
+  const { localPlusCode, address } = splitLocalPlusCode(plusCode);
+  if (!localPlusCode || !address) {
+    return "";
+  }
+  if (!OpenLocationCode.isValid(localPlusCode)) {
+    return "";
+  }
+  const base = await queryGsi(address);
+
+  const { latitudeCenter, longitudeCenter } = OpenLocationCode.decode(
+    OpenLocationCode.recoverNearest(localPlusCode, base.lat, base.lon)
+  );
+  // https://map.yahoo.co.jp/blog/archives/20150202_carnavischeme.html
+  return `yjcarnavi://navi/select?lat=${latitudeCenter}&lon=${longitudeCenter}`;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [plusCodeInput, setPlusCodeInput] = useState<string>("");
+  const [yjcarnaviUrl, setYjcarnaviUrl] = useState<string>("");
+
+  const onSetPlusCodeInput = async (plusCode: string) => {
+    setPlusCodeInput(plusCode);
+    setYjcarnaviUrl(await yjcarnaviUrlFromPlusCode(plusCode));
+  };
 
   return (
     <>
+      <input
+        type="text"
+        value={plusCodeInput}
+        onChange={(e) => onSetPlusCodeInput(e.target.value)}
+      />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <a href={yjcarnaviUrl}>Yahoo! カーナビ で開く</a>
+        <br />
+        <small>{yjcarnaviUrl}</small>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
